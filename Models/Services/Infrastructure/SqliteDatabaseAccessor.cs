f4f1206cf67b0d11bddc8455b87b4b3f2aba948e
SimoneSantoro <s.santoro110900@gmail.com>
2023-08-29 17:55:16 +0200

    Terminata sezione 10

--- Models/Services/Infrastructure/SqliteDatabaseAccessor.cs
+++ Models/Services/Infrastructure/SqliteDatabaseAccessor.cs
@@ -9,20 +9,36 @@ namespace MyCourse.Models.Services.Infrastructure
 {
     public class SqliteDatabaseAccessor : IDataBaseAccessor
     {
-        public DataSet Query(string query)
+        public async Task<DataSet> QueryAsync(FormattableString formattableQuery)
         {
+            var queryArguments = formattableQuery.GetArguments();
+            var sqliteParameters = new List<SqliteParameter>();
+            for(int i=0; i<queryArguments.Length;i++)
+            {
+                var parameter = new SqliteParameter(i.ToString(), queryArguments[i]);
+                sqliteParameters.Add(parameter);
+                queryArguments[i] = "@" + i;
+            }
+            string query = formattableQuery.ToString();
+
             using(var conn = new SqliteConnection("Data Source=Data/MyCourse.db"))
             {
-                conn.Open();
+                await conn.OpenAsync();
                 using(var cmd = new SqliteCommand(query, conn))
                 {
-                    using(var reader = cmd.ExecuteReader())
+                    cmd.Parameters.AddRange(sqliteParameters);
+                    using(var reader = await cmd.ExecuteReaderAsync())
                     {
                         var dataSet = new DataSet();
                         dataSet.EnforceConstraints = false;
-                        var dataTable = new DataTable();
-                        dataSet.Tables.Add(dataTable);
-                        dataTable.Load(reader);
+
+                        do
+                        {
+                            var dataTable = new DataTable();
+                            dataSet.Tables.Add(dataTable);
+                            dataTable.Load(reader);
+                        }while(!reader.IsClosed);
+
                         return dataSet;
                     };
 
